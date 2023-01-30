ad280e07e356620e9eaa5ca7d8f72bc9d7042bc6
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-01-30 20:32:16 +0000

    update babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-     entry: './js/script.js',
+     entry: ['babel-polyfill', './js/script.js'],
      output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'js/bundle.js'
      },
      devServer: {
         contentBase: './dist'
+     },
+     plugins: [
+      new HtmlWebpackPlugin({
+         filename: 'index.html',
+         template: './index.html'
+      })
+     ],
+     module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: /non_modules/,
+            use:{
+               loader: 'babel-loader'
+            }
+         }
+      ]
      }
 };
